ffce42f748ce4748eb3cf5c3f1696ef78385cce0
r0zmarin1 <IRozina2006@gmail.com>
2020-10-15 19:08:26 +1000

    101010

--- main.py
+++ main.py
@@ -9,13 +9,36 @@ H = 360
 SILVER = (192, 192, 192)
 BLACK = (0, 0, 0,)
 
+# создаем окна
 pygame.init()
-pygame.display.set_caption('угадай число')
+pygame.display.set_caption('клуб фотографий')
 pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((W, H))
 
+# создаем фон
+bg = pygame.image.load('Image/room.png')
+bg_rect = bg.get_rect(topleft=(0, 0))
+cat = pygame.image.load('Image/cat.png')
+cat_rect = cat.get_rect(center=(70, 220))
+dog = pygame.image.load('Image/dog.png')
+dog_rect = dog.get_rect(center=(410, 220))
+owl = pygame.image.load('Image/owl.png')
+owl_rect = owl.get_rect(center=(210, 120))
+dialog = pygame.image.load('Image/dialog.png')
+dialog_rect = dialog.get_rect()
+
+# возможность закрывать окно через esc
 run = True 
 while run:
     for e in pygame.event.get():
-        if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+        if e.type == pygame.QUIT:
             run = False
+        elif e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+                run = False
+
+    screen.blit(bg, bg_rect)
+    screen.blit(cat, cat_rect)
+    screen.blit(dog, dog_rect)
+    screen.blit(owl, owl_rect)
+    pygame.display.update()
