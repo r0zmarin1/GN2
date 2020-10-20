5b0ee29734ec3269e2b5ad7d76e2683ac6c772fa
r0zmarin1 <IRozina2006@gmail.com>
2020-10-20 19:13:28 +1000

    fjfjfj

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ W = 480
 H = 360
 SILVER = (192, 192, 192)
 BLACK = (0, 0, 0,)
+numeral = ''
+move = 1
+block = 0
+start = 1
+OUTSIZE_BG = (0, -100)
 
 # создаем окна
 pygame.init()
@@ -26,9 +31,29 @@ owl = pygame.image.load('Image/owl.png')
 owl_rect = owl.get_rect(center=(210, 120))
 dialog = pygame.image.load('Image/dialog.png')
 dialog_rect = dialog.get_rect()
+dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
+dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
+dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+
+# разбираемся со шрифтами
+font = pygame.font.SysFont('Arial', 28, True, False)
+font2 = pygame.font.SysFont('Arial', 14, False, True)
+font_box = pygame.Surface((W - 30, font.get_height()))
+font_box_rect = font_box.get_rect(center=(W // 2, H - 30))
+
+
+# рисуем диалоги
+def dialogs(text, pos, owl_text):
+    screen.blit(dialog, pos)
+    screen.blit(font2.render(text, True, BLACK), (pos[0] + 5, pos[1] + 5))
+    screen.blit(dialog, dialog_owl_pos)
+    screen.blit(font2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5, dialog_owl_pos[1] + 5))
+    pygame.display.update()
+    pygame.time.wait(2000)
+
 
 # возможность закрывать окно через esc
-run = True 
+run = True
 while run:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
@@ -37,8 +62,12 @@ while run:
             if e.key == pygame.K_ESCAPE:
                 run = False
 
-    screen.blit(bg, bg_rect)
-    screen.blit(cat, cat_rect)
-    screen.blit(dog, dog_rect)
-    screen.blit(owl, owl_rect)
+# показ спрайтов и фонов на экране
+    if block == 0:
+        screen.blit(bg, bg_rect)
+        screen.blit(cat, cat_rect)
+        screen.blit(dog, dog_rect)
+        screen.blit(owl, owl_rect)
+        screen.blit(font_box, font_box_rect)
+        font_box.fill(SILVER)
     pygame.display.update()
